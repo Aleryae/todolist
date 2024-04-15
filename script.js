ecbe66e4960b8891f1e8533ed492e5a0d5ae7027
Aleryae <74616540+Aleryae@users.noreply.github.com>
2024-04-15 14:46:27 +0200

    create tasks

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 console.log("Ready !");
 window.addEventListener("load", () => {
-	console.log("Hello world !");
+	console.log("Welcome to the to do list");
 
+	class Task {
+		constructor(content) {
+			this.content = content;
+		}
 
+		createInput(parent) {
+			const inputGroup = document.createElement('div');
+			inputGroup.setAttribute('class', 'input-group mb-3');
 
+			const taskOutput = document.createElement('div');
+			taskOutput.setAttribute('class', 'input-group-text');
+
+			const inputCheck = document.createElement('input');
+			inputCheck.setAttribute('class', 'form-check-input mt-0');
+			inputCheck.setAttribute('type', 'checkbox');
+			inputCheck.setAttribute('aria-label', 'Checkbox for following text input');
+			inputCheck.setAttribute('value', '');
+
+			const inputContent = document.createElement('input');
+			inputContent.setAttribute('class', 'form-control bg-dark');
+			inputContent.setAttribute('type', 'text');
+			inputContent.setAttribute('aria-label', 'Text input with checkbox');
+			inputContent.setAttribute('value', this.content);
+			inputContent.setAttribute('disabled', '');
+			inputContent.setAttribute('readonly', '');
+
+			parent.appendChild(inputGroup);
+			inputGroup.appendChild(taskOutput);
+			taskOutput.appendChild(inputCheck);
+			inputGroup.appendChild(inputContent);
+
+		}
+	}
+	let group = document.querySelector('#group');
+	let taskInput = document.querySelector('#taskInput');
+
+	taskInput.addEventListener('keypress', function (e) {
+		if (e.key === 'Enter') {
+			const task = new Task(taskInput.value);
+			task.createInput(group);
+		}
+	});
 
 });
